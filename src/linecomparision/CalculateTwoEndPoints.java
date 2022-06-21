2099836d3dd4e97c7cd38958c886f999f1d16490
priya <priyapandey8884.blr@gmail.com>
2022-06-21 12:01:02 +0530

    Welcome Message

---
+++ src/linecomparision/CalculateTwoEndPoints.java
@@ -0,0 +1,11 @@
+package linecomparision;
+
+public class CalculateTwoEndPoints {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparision Computation");
+		// TODO Auto-generated method stub
+
+	}
+
+}
