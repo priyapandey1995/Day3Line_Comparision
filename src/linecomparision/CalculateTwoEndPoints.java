62632ef61cbc0a20df30ffa469fa796c24a00f0d
priya <priyapandey8884.blr@gmail.com>
2022-06-21 17:52:05 +0530

    tofindgreter_lesser_equals

--- src/linecomparision/CalculateTwoEndPoints.java
+++ src/linecomparision/CalculateTwoEndPoints.java
@@ -5,14 +5,14 @@ public class CalculateTwoEndPoints {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparision Computation");
 		// TODO Auto-generated method stub
-		int x1=8;
-		int x2=8;
-		int y1=7;
-		int y2=7;
-		int x3=6;
-		int x4=6;
-		int y3=7;
-		int y4=7;
+		int x1=4;
+		int x2=1;
+		int y1=8;
+		int y2=2;
+		int x3=5;
+		int x4=8;
+		int y3=5;
+		int y4=6;
 		
 		double distanceOfLine1 = (Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2)));
 		double distanceOfLine2 = (Math.sqrt(Math.pow(x4-x3,2)+Math.pow(y4-y3,2)));
@@ -21,14 +21,15 @@ public class CalculateTwoEndPoints {
 		 Integer obj1 = new Integer((int)distanceOfLine1); 
 		 Integer obj2 = new Integer((int)distanceOfLine2);
 		  System.out.println("these lines are equal" + " " +obj1.equals(obj2));
+		  if(obj1.compareTo(obj2) > 0)
+		  {
+			  System.out.println("length of first line is greater");
+		  }
+		  else
+		  {
+			  System.out.println("length of second line is greter");
+		  }
 				 
-		
-			
-			
-		
-	
-		
-
 	}
 
 }
