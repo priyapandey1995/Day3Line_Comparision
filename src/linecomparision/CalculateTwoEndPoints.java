233a594d6337bea6f9ba9ff952f0fcc5b3684f8a
priya <priyapandey8884.blr@gmail.com>
2022-06-21 12:46:53 +0530

    Checking_Length_Of_TwoEnds_UC1

--- src/linecomparision/CalculateTwoEndPoints.java
+++ src/linecomparision/CalculateTwoEndPoints.java
@@ -5,6 +5,14 @@ public class CalculateTwoEndPoints {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparision Computation");
 		// TODO Auto-generated method stub
+		int x1=2;
+		int x2=8;
+		int y1=8;
+		int y2=2;
+		//formula:sqrt(x2-x1)^2 + (y2-y1)^2
+		//double distance=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+		double distance=Math.floor(Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2)));
+		System.out.println(distance);
 
 	}
 
